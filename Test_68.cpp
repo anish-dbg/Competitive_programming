3152bea139dd37ba8bdc67a529166318c904dd0f
anish <anishkr0098@gmail.com>
2026-06-17 16:00:45 +0530

    prefix-ques

---
+++ Test_68.cpp
@@ -0,0 +1,36 @@
+// Brute force
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>> t;
+    while(t--){
+    int n,q;
+    cin>>n>>q;
+    string s;
+    cin>>s;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        int hsh[26];
+        for(int i=0; i<26; i++){
+            hsh[i] = 0;
+        }
+        l--; r--;
+        for(int i=l; i<=r; i++){
+            hsh[s[i] - 'a']++;
+        }
+        int oddCt = 0;
+        for(int i=0; i<26; i++){
+            if(hsh[i]%2 != 0) oddCt++;
+        }
+        if(oddCt > 1) cout <<"No\n";
+        else cout<<"Yes\n";
+     }
+    }
+}
+
+// O(t*(n+q*(26+n+26)))
